c64b2fcf50e94153d2c1703247f3a0de3c474e6d
SungGil-5125 <sunggil0125@naver.com>
2022-04-27 17:00:13 +0900

    Add ServletComponentScan

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan //하위 패키지를 포함해 서블렛을 찾아줘서 서블렛을 등록한다 (서블렛 자동등록)
 @SpringBootApplication
 public class ServletApplication {
 
