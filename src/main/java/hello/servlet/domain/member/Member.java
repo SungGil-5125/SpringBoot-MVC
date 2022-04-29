52563587b207e99eb9d4a9b1d96749cf3300d9d3
SungGil-5125 <sunggil0125@naver.com>
2022-04-29 19:45:52 +0900

    Member(id, username, age)

---
+++ src/main/java/hello/servlet/domain/member/Member.java
@@ -0,0 +1,22 @@
+package hello.servlet.domain.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    private Long id;
+    private String username;
+    private int age;
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}
